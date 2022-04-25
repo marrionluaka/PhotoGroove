ca510107481aff1cc917ba1340a21f00cb6eea22
Marrion Lulu <marrion91300@gmail.com>
2022-04-25 09:04:49 -0700

    Adding model + selected url

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -4,16 +4,58 @@ import Html exposing (div, h1, img, text)
 import Html.Attributes exposing (..)
 
 
+main : Html.Html msg
+main =
+    view initialModel
+
+
+
+-- Data
+
+
+type alias Model =
+    { photos : List { url : String }
+    , selectedUrl : String
+    }
+
+
+initialModel : Model
+initialModel =
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    }
+
+
+
+-- View
+
+
+urlPrefix : String
+urlPrefix =
+    "http://elm-in-action.com/"
+
+
+view : Model -> Html.Html msg
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ]
-            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-            , img [ src "http://elm-in-action.com/2.jpeg" ] []
-            , img [ src "http://elm-in-action.com/3.jpeg" ] []
+        , div [ id "thumbnails" ] (List.map (\photo -> viewThumbnail model.selectedUrl photo) model.photos)
+        , img
+            [ class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
             ]
+            []
         ]
 
 
-main =
-    view "no model yet"
+viewThumbnail : String -> { url : String } -> Html.Html msg
+viewThumbnail selectedUrl thumb =
+    img
+        [ src (urlPrefix ++ thumb.url)
+        , classList [ ( "selected", selectedUrl == thumb.url ) ]
+        ]
+        []
